46864cedd07d6204efc615d8da9d8ee12e70a4c3
Anushar1821998 <anushar1821998@gmail.com>
2025-06-12 07:00:22 +0530

    50+ practice codes

---
+++ src/ex_04_Operator/Lab046_Interview.java
@@ -0,0 +1,7 @@
+package ex_04_Operator;
+
+public class Lab046_Interview {
+    public static void main(String[] args) {
+        System.out.println(!(10>20));
+    }
+}
---
+++ src/ex_04_Operator/Lab050_New_Operator.java
@@ -0,0 +1,9 @@
+package ex_04_Operator;
+
+public class Lab050_New_Operator {
+    public static void main(String[] args) {
+        // New Operator - oops - creates memory
+        String name = new String("Pramod");
+        // Learn this in OOPs
+    }
+}
