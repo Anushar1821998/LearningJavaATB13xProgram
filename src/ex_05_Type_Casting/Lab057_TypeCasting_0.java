99141919bdfe9e8e5c4b536946d3d54254641109
Anushar1821998 <anushar1821998@gmail.com>
2025-06-15 22:04:34 +0530

    Tasks

---
+++ src/ex_05_Type_Casting/Lab057_TypeCasting_0.java
@@ -0,0 +1,8 @@
+package ex_05_Type_Casting;
+
+public class Lab057_TypeCasting_0 {
+    public static void main(String[] args) {
+        byte a = 10;
+        int b = a; //implicit casting -->widening
+    }
+}
