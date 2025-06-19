20b50ab824d756e9cbd173d3dd9e9830796c3211
Anushar1821998 <anushar1821998@gmail.com>
2025-06-19 08:01:23 +0530

    Test

---
+++ src/Test/Print_Values.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Print_Values {
+    public static void main(String[] args) {
+        int a= 25;
+        double d = 3.14;
+        boolean b = true;
+        char c = 'A';
+
+        System.out.println (a);
+        System.out.println (d);
+        System.out.println (b);
+        System.out.println (c);
+    }
+}
