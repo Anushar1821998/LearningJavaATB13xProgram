ae429058977b2f237199dc682235182831082222
Anushar1821998 <anushar1821998@gmail.com>
2025-06-19 11:13:25 +0530

    Test

---
+++ src/Test/Challenge_5_ExpImp.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class Challenge_5_ExpImp {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Test/Print_Values.java
+++ src/Test/Print_Values.java
@@ -2,6 +2,9 @@ package Test;
 
 public class Print_Values {
     public static void main(String[] args) {
+        //Write a Java program that demonstrates proper variable declaration and initialization for different data types.
+        // **Requirements:** - Declare and initialize variables of different primitive data types (int, double, boolean, char)
+        // - Print the values of all variables - Show proper naming conventions
         int a= 25;
         double d = 3.14;
         boolean b = true;
