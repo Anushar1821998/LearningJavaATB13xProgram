3821242581478bdb77026f42b86795f7be197a15
Anushar1821998 <anushar1821998@gmail.com>
2025-06-23 06:39:57 +0530

    17th JUne

---
+++ src/ex_11_While/Lab118_While.java
@@ -0,0 +1,10 @@
+package ex_11_While;
+
+public class Lab118_While {
+    public static void main(String[] args) {
+        //Process finished with exit code 130
+        while (true){
+            System.out.println("Hello, Pramod");
+        }
+    }
+}
