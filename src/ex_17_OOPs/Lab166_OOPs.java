6bd1464812ff38dda58a06f8e93ae6add5a5882e
Anushar1821998 <anushar1821998@gmail.com>
2025-07-14 16:01:35 +0530

    30th June

---
+++ src/ex_17_OOPs/Lab166_OOPs.java
@@ -0,0 +1,13 @@
+package ex_17_OOPs;
+
+public class Lab166_OOPs {
+    public static void main(String[] args) {
+
+        person p1 = new person();
+        person p2 = new person();
+
+        System.out.println(p1.age);
+        p2.walk();
+
+    }
+}
